2ba8f06571edbf9ba7c578444b6152369c3a97c3
ageyssel <ageyssel.llanos@gmail.com>
2026-04-15 10:21:52 -0400

    Eliminar regla eslint obsoleta de next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Esto es la magia que Cloudflare necesita
-  eslint: {
-    ignoreDuringBuilds: true, // Evita que errores menores de texto frenen el despliegue
-  },
+  output: 'export', 
 };
 
 export default nextConfig;
